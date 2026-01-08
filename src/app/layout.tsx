dbb9c5c99e4baa188bf1ee1d74f4c6d9ac657b0e
Web <webd75369@gmail.com>
2026-01-08 20:15:08 +0530

    added shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +15,10 @@ export default function RootLayout({ children }: child) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>{children}</main>
+        <main>
+          {children}
+          <Toaster />
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
 export default function Home() {
+  const handleClick = () => toast.success("This button is clicked");
   return (
     <div className="p-4">
-      <p className="text-neutral-500 font-light">Home</p>
+      <Button onClick={handleClick}>Click Me</Button>
     </div>
   );
 }
