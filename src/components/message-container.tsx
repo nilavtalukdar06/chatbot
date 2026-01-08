ed0af7b3839eb2a1895922f57b7edfcf767420e7
Web <webd75369@gmail.com>
2026-01-08 23:51:58 +0530

    added scroll area

--- src/components/message-container.tsx
+++ src/components/message-container.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { PromptInput } from "./prompt-input";
+import { ScrollArea } from "./ui/scroll-area";
 
 export function MessageContainer() {
   return (
     <div className="flex flex-col flex-1 min-h-0">
-      <div className="flex-1 flex flex-col gap-y-4"></div>
+      <ScrollArea className="flex-1 flex flex-col gap-y-4"></ScrollArea>
       <PromptInput />
     </div>
   );
