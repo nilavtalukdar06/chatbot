acb98b0f252a68d12538a0bd18cd3a4bbfb03ba5
Web <webd75369@gmail.com>
2026-01-09 15:57:12 +0530

    minor code updates

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,16 +3,6 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
-  user: {
-    additionalFields: {
-      status: {
-        type: ["free_user", "pro_user"],
-        required: true,
-        defaultValue: "free_user",
-        input: false,
-      },
-    },
-  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
