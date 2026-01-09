1ba7d5dfa46d868d16d103f7403572933698aa1e
Web <webd75369@gmail.com>
2026-01-09 17:27:50 +0530

    added client side checkout

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -8,9 +8,32 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import Link from "next/link";
+import { toast } from "sonner";
+import { authClient } from "@/utils/auth-client";
 
 export function Menu() {
+  const { data: session } = authClient.useSession();
+  const openPortal = async () => {
+    try {
+      await authClient.customer.portal();
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to open customer portal");
+    }
+  };
+
+  const checkout = async () => {
+    try {
+      await authClient.checkout({
+        products: ["6659225f-fee6-4089-9d03-170b70757f77"],
+        slug: "Chatbot-Pro-Plan",
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to checkout");
+    }
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -21,12 +44,10 @@ export function Menu() {
       <DropdownMenuContent>
         <DropdownMenuLabel>Settings</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Link href="/billing">Billing</Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Link href="/upgrade">Upgrade</Link>
-        </DropdownMenuItem>
+        <DropdownMenuItem onClick={openPortal}>Billing</DropdownMenuItem>
+        {session?.user.status === "free_user" && (
+          <DropdownMenuItem onClick={checkout}>Upgrade</DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 import { polarClient } from "@polar-sh/better-auth";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL!,
-  plugins: [polarClient()],
+  plugins: [polarClient(), inferAdditionalFields<typeof auth>()],
 });
