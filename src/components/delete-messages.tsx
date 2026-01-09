a9b1564931576feef8e59ae2fc89e8bba6cb3c37
Web <webd75369@gmail.com>
2026-01-09 10:54:30 +0530

    added delete functionality

--- src/components/delete-messages.tsx
+++ src/components/delete-messages.tsx
@@ -2,11 +2,40 @@
 
 import { TriangleAlertIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+import { Spinner } from "./ui/spinner";
 
 export function DeleteMessages() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    trpc.message.deleteMany.mutationOptions({
+      onSuccess: () => {
+        toast.success("Deleted Messages");
+        queryClient.invalidateQueries({
+          queryKey: trpc.message.getMany.queryKey(),
+        });
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
   return (
-    <Button size="icon-sm" variant="outline" className="bg-white">
-      <TriangleAlertIcon className="text-red-500" />
+    <Button
+      size="icon-sm"
+      variant="outline"
+      className="bg-white"
+      disabled={mutation.isPending}
+      onClick={() => mutation.mutate()}
+    >
+      {mutation.isPending ? (
+        <Spinner className="text-muted-foreground" />
+      ) : (
+        <TriangleAlertIcon className="text-muted-foreground" />
+      )}
     </Button>
   );
 }
